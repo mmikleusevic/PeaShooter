afecd2f3e80e396aae481fddcc2de39a886d01fb
mmikleusevic <mikleusevicmarin@gmail.com>
2024-06-24 10:20:51 +0200

    Changed movement system, now rotating a bit differently, also moved the pivot of the player to the center of the object, added basic ground material to plane

--- Assets/Scripts/Systems/PlayerMovementSystem.cs
+++ Assets/Scripts/Systems/PlayerMovementSystem.cs
@@ -1,7 +1,6 @@
 using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
-using UnityEngine;
 
 [BurstCompile]
 public partial struct PlayerMovementSystem : ISystem
@@ -14,16 +13,27 @@ public partial struct PlayerMovementSystem : ISystem
         PlayerMovementAspect playerMovement = SystemAPI.GetAspect<PlayerMovementAspect>(player);
 
         float3 moveDirection = new float3(playerMovement.input.ValueRO.move.x, 0, playerMovement.input.ValueRO.move.y);
-        playerMovement.physics.ValueRW.Linear += moveDirection * playerMovement.playerController.ValueRO.moveSpeed * SystemAPI.Time.DeltaTime;
-        playerMovement.playerController.ValueRW.moveDirection = moveDirection;
 
         if (math.lengthsq(moveDirection) > 0f)
         {
-            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0, moveDirection.z), Vector3.up);
+            playerMovement.physics.ValueRW.Linear += moveDirection * playerMovement.playerController.ValueRO.moveSpeed * SystemAPI.Time.DeltaTime;
 
-            playerMovement.transform.ValueRW.Rotation = Quaternion.Slerp(playerMovement.transform.ValueRO.Rotation, 
-                lookRotation, playerMovement.playerController.ValueRO.rotationSpeed * SystemAPI.Time.DeltaTime);
+            quaternion targetRotation = quaternion.LookRotationSafe(new float3(moveDirection.x, 0, moveDirection.z), math.up());
+            quaternion currentRotation = playerMovement.transform.ValueRO.Rotation;
+            quaternion rotationDifference = math.mul(targetRotation, math.inverse(currentRotation));
+
+            float3 eulerDifference = math.degrees(new float3(math.asin(rotationDifference.value.x) * 2, math.asin(rotationDifference.value.y) * 2, math.asin(rotationDifference.value.z) * 2));
+            float3 angularVelocity = new float3(0, eulerDifference.y, 0) * playerMovement.playerController.ValueRO.rotationSpeed * SystemAPI.Time.DeltaTime;
+
+            playerMovement.physics.ValueRW.Angular = angularVelocity;
+            playerMovement.transform.ValueRW.Rotation = math.slerp(currentRotation, targetRotation, playerMovement.playerController.ValueRO.rotationSpeed * SystemAPI.Time.DeltaTime);
         }
+        else
+        {
+            playerMovement.physics.ValueRW.Angular = 0;
+        }
+
+        playerMovement.playerController.ValueRW.moveDirection = moveDirection;
 
         float3 currentPosition = playerMovement.transform.ValueRO.Position;
 
