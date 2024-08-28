b3323542516363507805e10a41df972017a0c9be
mmikleusevic <mikleusevicmarin@gmail.com>
2024-08-28 13:43:57 +0200

    Updated unity, added components for new ability system

---
+++ Assets/Scripts/Components/AbilityComponent.cs
@@ -0,0 +1,9 @@
+using Unity.Entities;
+
+public struct AbilityComponent : IComponentData
+{
+    public float cooldown;
+    public float cooldownRemaining;
+    public int damage;
+    public float range;
+}
---
+++ Assets/Scripts/Components/ProjectileComponent.cs
@@ -0,0 +1,7 @@
+using Unity.Entities;
+
+public struct ProjectileComponent : IComponentData
+{
+    public float Speed;
+    public float Lifetime;
+}
